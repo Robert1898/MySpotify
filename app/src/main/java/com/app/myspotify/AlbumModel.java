bb2aa125231166aa4ea06e7854d32aaac859d863
Robert1898 <mirza_robert@yahoo.com>
2024-09-30 16:42:44 +0300

    UI modified

---
+++ app/src/main/java/com/app/myspotify/AlbumModel.java
@@ -0,0 +1,4 @@
+package com.app.myspotify;
+
+public class AlbumModel {
+}
--- app/src/main/java/com/app/myspotify/MainActivity.java
+++ app/src/main/java/com/app/myspotify/MainActivity.java
@@ -9,8 +9,12 @@ import com.spotify.protocol.types.Track;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 }
