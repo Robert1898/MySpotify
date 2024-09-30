2685f7bc9c1bd9a7f990e4422025b4ca7f4ba4b8
Robert1898 <mirza_robert@yahoo.com>
2024-09-30 14:27:53 +0300

    test

---
+++ app/src/main/java/com/app/myspotify/MainActivity.java
@@ -0,0 +1,23 @@
+package com.app.myspotify;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
